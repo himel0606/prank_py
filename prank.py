d47f6aa3e77dbf5b6862cc7f0285bf313a9286a1
Himel das <himeldas06bd@gmail.com>
2025-11-11 00:39:36 +0600

    Add files via upload

---
+++ prank.py
@@ -0,0 +1,7 @@
+
+import tkinter
+root = tkinter.Tk()
+root.attributes('-fullscreen', True)
+root.attributes('-topmost', True)
+root.protocol("WM_DELETE_WINDOW", lambda: None)  # Disable close button
+root.mainloop()
